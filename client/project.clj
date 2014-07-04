5f421b813dc4373b37592a578a6045fa5ef93952
Dan Lentz <danlentz@gmail.com>
2014-07-04 13:56:19 -0400

    updated hornetq dependencies

--- client/project.clj
+++ client/project.clj
@@ -1,10 +1,10 @@
-(defproject hornetq-clj/client "0.2.1"
+(defproject hornetq-clj/client "0.2.1-LENTZ"
   :description "Simplify using hornetq server"
   :url "https://github.com/hugoduncan/hornetq-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.hornetq/hornetq-core-client "2.2.18.Final"]
+                 [org.hornetq/hornetq-core-client "2.3.17.Final"]
                  [org.jboss.netty/netty "3.2.1.Final"]]
   :repositories {"JBoss releases"
                  "http://repository.jboss.org/nexus/content/groups/public-jboss/"})
